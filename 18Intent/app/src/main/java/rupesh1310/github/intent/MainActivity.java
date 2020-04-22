8eba049c420846b4b53564140ac545f3d0f584e7
rupesh1310 <a.rupesh66@gmail.com>
2020-04-22 19:49:11 +0530

    :rocket: passing multiple values from intent

--- 18Intent/app/src/main/java/rupesh1310/github/intent/MainActivity.java
+++ 18Intent/app/src/main/java/rupesh1310/github/intent/MainActivity.java
@@ -5,18 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edittext);
     }
 
     public void screenone(View view){
-        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-        startActivity(intent);
+        String value = editText.getText().toString();
+
+        if(value.length() == 0) {
+         editText.setError("No value");
+        }
+        else
+        {
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+//            intent.putExtra("name", value);
+//            intent.putExtra("password", value + " new");
+
+             Bundle bundle = new Bundle();
+             bundle.putString("name", value);
+             bundle.putString("password", value + " new!");
+
+             intent.putExtras(bundle);
+             // Array list
+             startActivity(intent);
+        }
+
+
     }
 }
