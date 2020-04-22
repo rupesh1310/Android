4d48f9161df0460836084ebac7628de6d1b11aa5
rupesh1310 <a.rupesh66@gmail.com>
2020-04-22 09:56:33 +0530

    :zap:  added quick changer

---
+++ 12QuickChanger/app/src/main/java/rupesh1310/github/quickchanger/MainActivity.java
@@ -0,0 +1,40 @@
+package rupesh1310.github.quickchanger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout layout;
+    RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+        radioGroup = findViewById(R.id.rgroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedID) {
+                switch (checkedID) {
+                    case R.id.rbtnblue:
+                        layout.setBackgroundColor(Color.parseColor("#74B9FF"));
+                        break;
+                    case R.id.rbtngreen:
+                        layout.setBackgroundColor(Color.parseColor("#45CE30"));
+                        break;
+                    case R.id.rbtnred:
+                        layout.setBackgroundColor(Color.parseColor("#FF3E4D"));
+                        break;
+
+                }
+            }
+        });
+    }
+}
