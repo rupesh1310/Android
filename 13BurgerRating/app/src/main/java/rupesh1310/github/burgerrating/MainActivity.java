9d0ff0f1cf6930ad7c5a63ee9c53cbba8e38022a
rupesh1310 <a.rupesh66@gmail.com>
2020-04-22 10:49:22 +0530

    :zap:  added burger rating app ui v2

--- 13BurgerRating/app/src/main/java/rupesh1310/github/burgerrating/MainActivity.java
+++ 13BurgerRating/app/src/main/java/rupesh1310/github/burgerrating/MainActivity.java
@@ -3,12 +3,37 @@ package rupesh1310.github.burgerrating;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RatingBar ratingBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBar);
+        textView = findViewById(R.id.textView);
+
+    }
+    public void submit(View view) {
+
+    float ratingValue = ratingBar.getRating();
+    if (ratingValue < 2) {
+        textView.setText("Rating: " + ratingValue + "\n We will try better next time");
+        }
+            else if(ratingValue <= 3 && ratingValue >=2) {
+
+        textView.setText("Rating: " + ratingValue + "\n We are constantly improving");
+         }
+    else if(ratingValue <=5) {
+
+        textView.setText("Rating: " + ratingValue + "\n Glad you loved it");
+    }
     }
 }
