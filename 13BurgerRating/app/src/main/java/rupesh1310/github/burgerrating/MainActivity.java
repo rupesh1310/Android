6640667d26b13f5bdff9190fd10cac2b436d85c4
rupesh1310 <a.rupesh66@gmail.com>
2020-04-22 10:15:58 +0530

    :zap:  added burger rating app ui

---
+++ 13BurgerRating/app/src/main/java/rupesh1310/github/burgerrating/MainActivity.java
@@ -0,0 +1,14 @@
+package rupesh1310.github.burgerrating;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
