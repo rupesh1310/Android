1cb4c079bdc09e236230e89befadbba6c1fcbb9c
rupesh1310 <a.rupesh66@gmail.com>
2020-04-22 14:04:00 +0530

    :rocket: added datePicker app v2

--- 16DateTimepicker/app/src/main/java/rupesh1310/github/datetimepicker/MainActivity.java
+++ 16DateTimepicker/app/src/main/java/rupesh1310/github/datetimepicker/MainActivity.java
@@ -3,13 +3,16 @@ package rupesh1310.github.datetimepicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
-
-    TimePicker timePicker;
+    DatePicker datePicker;
+    //TimePicker timePicker;
     TextView textView;
 
     @Override
@@ -17,15 +20,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timePicker = findViewById(R.id.timepicker);
-        textView = findViewById(R.id.textview);
 
-        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
-                textView.setText("Hour: " + hour+ "\n Minutes: " + minute);
-            }
-        });
+      //  timePicker = findViewById(R.id.timepicker);
+        textView = findViewById(R.id.textview);
+        datePicker = findViewById(R.id.datepicker);
+
+
+        Calendar calendar = Calendar.getInstance();
+
+        datePicker.init(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
+                new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker datePicker, int year, int month, int day){
+                        textView.setText("You selected: " + day + "-" + month + "-" + year);
+                    }
+                }
+        );
+
+
+
+
+
+
+
+
+
+
+//        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+//            @Override
+//            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
+//                textView.setText("Hour: " + hour+ "\n Minutes: " + minute);
+//            }
+//        });
 
     }
 }
