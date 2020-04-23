150bade204b700e1f9ed24e441e62df5371f7ae9
rupesh1310 <a.rupesh66@gmail.com>
2020-04-23 09:17:08 +0530

    :art:addded and remove card

--- 22RecyclerCards/app/src/main/java/rupesh1310/github/recyclercards/MainActivity.java
+++ 22RecyclerCards/app/src/main/java/rupesh1310/github/recyclercards/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<ExampleItem> exampleList;
+    private Button  buttonAdd, buttonDelete;
+    private EditText editTextAdd, editTextDelete;
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
@@ -24,6 +29,33 @@ public class MainActivity extends AppCompatActivity {
 
         generateFakeData();
         recyclerViewConfig();
+
+        //find buttons
+        buttonAdd = findViewById(R.id.buttonadd);
+        buttonDelete = findViewById(R.id.buttondelete);
+
+        //find edittexts
+        editTextAdd = findViewById(R.id.edittextadd);
+        editTextDelete = findViewById(R.id.edittextdelete);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: make sure user is not allowed to submit empty form
+                int position = Integer.parseInt(editTextAdd.getText().toString());
+                addCard(position);
+            }
+        });
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = Integer.parseInt(editTextDelete.getText().toString());
+                deleteCard(position);
+            }
+        });
+
+
+
 
     }
 
@@ -54,5 +86,15 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
 
+    }
+
+    public void addCard(int position){
+        exampleList.add(position, new ExampleItem(R.drawable.node, "new card added"));
+//        adapter.notifyDataSetChanged();
+        adapter.notifyItemInserted(position);
+    }
+    public void deleteCard(int position){
+        exampleList.remove(position);
+        adapter.notifyItemRemoved(position);
     }
 }
