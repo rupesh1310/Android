a8cb06419470023c33533ecfc4099d9127612cb5
rupesh1310 <a.rupesh66@gmail.com>
2020-04-23 18:48:11 +0530

    :card_file_box: helper for showing messages '

--- 23SqLiteDatabase/app/src/main/java/rupesh1310/github/sqllitestudentrecord/MainActivity.java
+++ 23SqLiteDatabase/app/src/main/java/rupesh1310/github/sqllitestudentrecord/MainActivity.java
@@ -1,14 +1,45 @@
 package rupesh1310.github.sqllitestudentrecord;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    DatabaseHelper myDB;
+    EditText editTextId, editName, editEmail, editCC;
+    Button buttonAdd, buttonGetData, buttonUpdate, buttonDelete, buttonViewAll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDB = new DatabaseHelper(this);
+        editTextId = findViewById(R.id.editText_id);
+        editName = findViewById(R.id.editText_name);
+        editEmail = findViewById(R.id.editText_email);
+;        editCC  = findViewById(R.id.editText_CC);
+        buttonAdd = findViewById(R.id.button_add);
+        buttonDelete = findViewById(R.id.button_delete);
+        buttonUpdate = findViewById(R.id.button_update);
+        buttonGetData = findViewById(R.id.button_view);
+        buttonViewAll = findViewById(R.id.button_viewAll);
+
+        showMessage("test","testing done");
+
+
+    }
+    private void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.create();
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+
     }
 }
