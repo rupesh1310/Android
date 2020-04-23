6f5d3536ac669522e987c0045dd8eb8555665f7b
rupesh1310 <a.rupesh66@gmail.com>
2020-04-23 06:38:35 +0530

    :rocket: added custom layouts &  getters

---
+++ 22RecyclerCards/app/src/main/java/rupesh1310/github/recyclercards/ExampleItem.java
@@ -0,0 +1,20 @@
+package rupesh1310.github.recyclercards;
+
+public class ExampleItem {
+
+    private int mImageResource;
+    private String mtext;
+
+    public ExampleItem(int imageResource, String text) {
+        mImageResource = imageResource;
+        mtext = text;
+    }
+
+    public int getmImageResource() {
+        return mImageResource;
+    }
+
+    public String getMtext() {
+        return mtext;
+    }
+}
