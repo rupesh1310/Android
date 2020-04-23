5ce124ac4480fc624f0c57281df7e786766c8a92
rupesh1310 <a.rupesh66@gmail.com>
2020-04-23 19:19:49 +0530

    :card_file_box: adding data in sqllite

--- 23SqLiteDatabase/app/src/main/java/rupesh1310/github/sqllitestudentrecord/MainActivity.java
+++ 23SqLiteDatabase/app/src/main/java/rupesh1310/github/sqllitestudentrecord/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,10 +31,28 @@ public class MainActivity extends AppCompatActivity {
         buttonGetData = findViewById(R.id.button_view);
         buttonViewAll = findViewById(R.id.button_viewAll);
 
-        showMessage("test","testing done");
-
-
     }
+    public void AddData(){
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                String myEmail = editEmail.getText().toString();
+                boolean isInserted = myDB.insertData(editName.getText().toString(), editEmail.getText().toString(), editCC.getText().toString());
+                if(isInserted == true){
+                     Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+        });
+    }
+
+
+
+
     private void showMessage(String title, String message){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.create();
