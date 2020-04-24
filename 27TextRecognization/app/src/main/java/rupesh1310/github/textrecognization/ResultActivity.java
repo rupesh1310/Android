75777d970ebdde907c6c59a4cb36467c732f9054
rupesh1310 <a.rupesh66@gmail.com>
2020-04-25 05:17:07 +0530

    :rocket: added resultActivity

--- 27TextRecognization/app/src/main/java/rupesh1310/github/textrecognization/ResultActivity.java
+++ 27TextRecognization/app/src/main/java/rupesh1310/github/textrecognization/ResultActivity.java
@@ -3,12 +3,32 @@ package rupesh1310.github.textrecognization;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private Button backButton;
+    private TextView resultTextView;
+    private  String resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        resultTextView = findViewById(R.id.result_textview);
+        backButton = findViewById(R.id.back_button);
+        resultText = getIntent().getStringExtra(LCOTextRecognization.RESULT_TEXT);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
 }
