8402c698c7f68cb8761bb385ec50a3ae9a516ffa
rupesh1310 <a.rupesh66@gmail.com>
2020-04-25 05:11:40 +0530

    :rocket: added textdetectionApp v1

---
+++ 27TextRecognization/app/src/main/java/rupesh1310/github/textrecognization/LCOTextRecognization.java
@@ -0,0 +1,16 @@
+package rupesh1310.github.textrecognization;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class LCOTextRecognization extends Application {
+
+    public  static  final  String RESULT_TEXT = "RESULT_TEXT";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
