de65b3db2f95d8393306fd4a7796aa1c24ced25a
rupesh1310 <a.rupesh66@gmail.com>
2020-04-24 16:54:08 +0530

    added changes to flogin

--- FireaseLogin/app/src/main/java/rupesh1310/github/fireaselogin/MainActivity.java
+++ FireaseLogin/app/src/main/java/rupesh1310/github/fireaselogin/MainActivity.java
@@ -1,10 +1,20 @@
 package rupesh1310.github.fireaselogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +22,51 @@ public class MainActivity extends AppCompatActivity {
     EditText password;
     Button button;
 
+    private FirebaseAuth mAuth;
+
     //Make sure to check your email & password are not empty & null
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Toast.makeText(this, "Already In", Toast.LENGTH_SHORT).show();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String myEmail = email.getText().toString();
-        String myPass = password.getText().toString();
 
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        
+        mAuth = FirebaseAuth.getInstance();
+    }
 
+    public void onRegister(View view) {
+        final String myEmail = email.getText().toString();
+        final String myPass = password.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(myEmail, myPass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.i("TAG", "createUserWithEmail:success");
+                            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Failure", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
 
     }
-}
+    }
+
