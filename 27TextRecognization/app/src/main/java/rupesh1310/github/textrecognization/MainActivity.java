d1b3648de8077259610cbd438a5a8ae2a423cfef
rupesh1310 <a.rupesh66@gmail.com>
2020-04-25 05:32:11 +0530

    :rocket firing up the camera

--- 27TextRecognization/app/src/main/java/rupesh1310/github/textrecognization/MainActivity.java
+++ 27TextRecognization/app/src/main/java/rupesh1310/github/textrecognization/MainActivity.java
@@ -1,14 +1,63 @@
 package rupesh1310.github.textrecognization;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button cameraButton;
+    private final static int REQUEST_CAMERA_CAPTURE = 124;
+    private FirebaseVisionTextRecognizer textRecognizer;
+    FirebaseVisionImage image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseApp.initializeApp(this);
+
+        cameraButton = findViewById(R.id.camera_button);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+                        startActivityForResult(takePictureIntent, REQUEST_CAMERA_CAPTURE);
+
+                }
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CAMERA_CAPTURE && requestCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            recognizeMyText(bitmap);
+        }
+    }
+
+    private void recognizeMyText(Bitmap bitmap) {
+
+        
+
     }
 }
